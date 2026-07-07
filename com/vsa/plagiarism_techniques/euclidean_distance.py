4dd7e60dfe1f57307938e7556f31a9eac53a4d7c
Abdul Rauf <abdul.rauf@ids-astra.com>
2026-07-07 17:42:43 +0500

    fix: extract scalar from similarity result for modern numpy

--- com/vsa/plagiarism_techniques/euclidean_distance.py
+++ com/vsa/plagiarism_techniques/euclidean_distance.py
@@ -4,18 +4,19 @@ Created on Mon Aug  5 09:23:58 2019
 
 @author: Administrator
 """
-from scipy.spatial import distance 
+from scipy.spatial import distance
+import numpy as np
 
 class Euclidean_Distance:
-    
-    
+
+
     def __init__(self):
         pass
-    
-    
+
+
     def test_palgiarism(self,dataset_1,dataset_2):
         self.dataset_1=dataset_1
         self.dataset_2=dataset_2
         if self.dataset_1 is not None and self.dataset_2 is not None:
             result=distance.euclidean(self.dataset_1,self.dataset_2)
-            return float(result)
+            return float(np.ravel(result)[0])
